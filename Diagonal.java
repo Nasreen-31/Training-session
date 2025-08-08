9ccbb53923f0bce2fb848eeb2fb803da824a2ad6
Nasreen-31 <nasreen3b@gmail.com>
2025-08-08 22:13:48 +0530

    Create Diagonal.java

---
+++ Diagonal.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Diagonal {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int n,r,c;
+        n=sc.nextInt();
+        n=n*2-1;
+        for(r=1;r<=n;r++){
+            for(c=1;c<=n;c++){
+                if(r==c || r+c==n+1)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
